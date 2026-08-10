f8c3dd5ebe82c999c5f0231235a9f1c9183fdb91
Firdos <wasilfirdos@gmail.com>
2026-08-10 11:50:16 +0300

    feat: add footer component to layout

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const AppLayout = ({ children }) => {
   return (
     <div className="app-layout">
       <Navbar />
       <main className="main-content">{children}</main>
-      <footer>
-        <p>Footer Placeholder</p>
-      </footer>
+      <Footer />
     </div>
   );
 };
